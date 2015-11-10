ea8e9866e5d7febaa5871a639002680622d0f008
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-11-09 23:45:09 -0600

    dungeon initialized

--- dungeon.rb
+++ dungeon.rb
@@ -6,6 +6,14 @@ class Dungeon
     @rooms = []
   end
 
+  def add_room(reference, name, description, connections)
+    @rooms << Room.new(reference, name, description, connections)
+  end
+
   Player = Struct.new(:name, :location)
   Room = Struct.new(:reference, :name, :description, :connections)
 end
+
+my_dungeon = Dungeon.new("Diarmuid") #initializes the dungeon with a player name
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave})
+my_dungeon.add_room(:smallcave, "Small Cave", "a small claustrophobic cave", {:east => :largecave})
